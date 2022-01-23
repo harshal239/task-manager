a6b192a09a2e20b8bfa2840fc50e4958fa4f39f6
harshal239 <harshalwalunj49@gmail.com>
2022-01-24 01:20:51 +0530

    make it better

--- server/index.js
+++ server/index.js
@@ -1,10 +1,10 @@
 const express = require("express");
-
 const app = express();
-const port = 4000;
+const port = process.env.PORT || 4000;
 const tasks = require("./routes/tasksRoutes");
 const connectDB = require("./db/connect");
 const cors = require("cors");
+const path = require("path");
 require("dotenv/config");
 
 //middlewares
@@ -12,9 +12,19 @@ app.use(cors());
 app.use(express.json());
 app.use("/api/v1/tasks", tasks);
 
-app.get("/", (req, res) => {
-  res.send("Home Page");
-});
+const __dirname1 = path.resolve();
+
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static(path.join(__dirname1, "/client/build/index.html")));
+
+  app.get("*", (req, res) =>
+    res.sendFile(path.resolve(__dirname1, "client", "build", "index.html"))
+  );
+} else {
+  app.get("/", (req, res) => {
+    res.send("API is running..");
+  });
+}
 
 const start = async () => {
   try {
